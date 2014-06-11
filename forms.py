d6561df34b8f54e11f4732ebd4fdf0e4560b9526
Harris Lapiroff <harris@chromamine.com>
2014-06-10 20:21:04 -0400

    Added user and request as hidden fields on the form.

--- forms.py
+++ forms.py
@@ -1,8 +1,12 @@
 from django import forms
+from django.core import serializers
 
 from zenaida.contrib.feedback.models import FeedbackItem
 
 class FeedbackForm(forms.ModelForm):
+    user = forms.IntegerField(widget=forms.HiddenInput)
+    request = forms.CharField(widget=forms.HiddenInput)
+
     class Meta:
-        fields = ('content', 'screenshot',)
+        fields = ('user', 'request', 'content', 'screenshot',)
         model = FeedbackItem
