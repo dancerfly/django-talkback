263019954a770e8c3f6c0a471e18b33f4dadcafa
Harris Lapiroff <harris@chromamine.com>
2014-06-18 23:07:40 -0400

    Form submission handling.

--- forms.py
+++ forms.py
@@ -1,19 +1,20 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.core import serializers
 
 from zenaida.contrib.feedback.models import FeedbackItem
 
 class FeedbackForm(forms.ModelForm):
-    user = forms.IntegerField(widget=forms.HiddenInput)
-    view = forms.CharField(widget=forms.HiddenInput)
-    content = forms.CharField(label="Your Message", widget=forms.Textarea)
-    request_path = forms.CharField(widget=forms.HiddenInput)
-    request_method = forms.CharField(widget=forms.HiddenInput)
-    request_encoding = forms.CharField(widget=forms.HiddenInput)
-    request_meta = forms.CharField(widget=forms.HiddenInput)
-    request_get = forms.CharField(widget=forms.HiddenInput)
-    request_post = forms.CharField(widget=forms.HiddenInput)
-    request_files = forms.CharField(widget=forms.HiddenInput)
+    user = forms.ModelChoiceField(queryset=get_user_model().objects.all(), widget=forms.HiddenInput, required=True)
+    view = forms.CharField(widget=forms.HiddenInput, required=True)
+    content = forms.CharField(label="Your Message", widget=forms.Textarea, required=True)
+    request_path = forms.CharField(widget=forms.HiddenInput, required=True)
+    request_method = forms.CharField(widget=forms.HiddenInput, required=True)
+    request_encoding = forms.CharField(widget=forms.HiddenInput, required=False)
+    request_meta = forms.CharField(widget=forms.HiddenInput, required=False)
+    request_get = forms.CharField(widget=forms.HiddenInput, required=False)
+    request_post = forms.CharField(widget=forms.HiddenInput, required=False)
+    request_files = forms.CharField(widget=forms.HiddenInput, required=False)
 
     def __init__(self, request=None, *args, **kwargs):
         super(FeedbackForm, self).__init__(*args, **kwargs)
@@ -28,6 +29,8 @@ class FeedbackForm(forms.ModelForm):
             'request_post': dict(request.POST),
             'request_files': dict(request.FILES),
         })
+
+    # TODO: Implement tampering validation on the user, view, and request_* fields
 
     class Meta:
         fields = (
--- views.py
+++ views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponseNotAllowed
+import json
+
+from django.http import (HttpResponse, HttpResponseNotAllowed,
+                         HttpResponseBadRequest)
+
+from zenaida.contrib.feedback.forms import FeedbackForm
 
 def feedback_ajax_submit(request):
-	if not request.POST:
-		return HttpResponseNotAllowed(['POST'])
+    if not request.POST:
+        return HttpResponseNotAllowed(['POST'])
+    else:
+        form = FeedbackForm(request, request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse(json.dumps({}), content_type="application/json")
+        else:
+            return HttpResponseBadRequest(form.errors.as_json(), content_type="application/json")
