ac42151fee785beacf27e4c4f79aa1fd69ec49d5
Harris Lapiroff <harris@chromamine.com>
2014-06-18 22:27:52 -0400

    Javascript to AJAX submit the form.

--- static/feedback/script.js
+++ static/feedback/script.js
@@ -1,15 +1,34 @@
 (function () {
 	"use strict";
 
-	var popupTab = $(".zf-popup-tab"),
-		popup = $("#ZenaidaFeedback");
+	var $popupTab = $(".zf-popup-tab"),
+		$popup = $("#ZenaidaFeedback"),
+		$form = $("#ZenaidaFeedback form"),
+		$popupBody = $(".zf-body"),
+		$submitButton = $("#ZenaidaFeedback form button"),
+		bind_submit = function () {
+			$form.one("submit", on_submit);
+		},
+		on_submit = function (e) {
+			$submitButton.attr("disabled", "disabled");
+			$submitButton.css({"opacity":.5});
+			$submitButton.text("Submitting...");
+			$.ajax({
+				type: $form.attr('method'),
+				url: $form.attr('action'),
+				data: $form.serialize(),
+				success: function (data) {
+					$popupBody.html("<div class='zf-thanks'>Thanks for your feedback!</div>")
+				}
+			});
+		};
 
-	popupTab.click(function () {
-		popup.toggleClass("in");
+	// toggle the feedback form when clicking on the tab
+	$popupTab.click(function () {
+		$popup.toggleClass("in");
 	});
 
-	$("#ZenaidaFeedback form").submit(function (e) {
-		e.preventDefault();
-	});
+	$form.on("submit", function (e) { e.preventDefault(); }); // always prevent the form from submitting
+	bind_submit(); // allow the form to make an ajax call only once
 
 }());
