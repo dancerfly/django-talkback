9c9f9cd5cdab67ae271632934c319b30ec233685
Harris Lapiroff <harris@chromamine.com>
2014-07-14 19:02:08 -0400

    Show unresolved feedback only by default.

--- admin.py
+++ admin.py
@@ -1,12 +1,43 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from zenaida.contrib.feedback.models import FeedbackItem
+
+class ResolvedListFilter(admin.SimpleListFilter):
+    title = _("resolved")
+    parameter_name = "resolved"
+
+    def lookups(self, request, modeladmin):
+        return (
+            ("all", _("All")),
+            (None, _("No")),
+            ("1", _("Yes"))
+        )
+
+    def choices(self, cl):
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': self.value() == lookup,
+                'query_string': cl.get_query_string({
+                    self.parameter_name: lookup,
+                }, []),
+                'display': title,
+            }
+
+    def queryset(self, request, queryset):
+        if self.value() == "all":
+            return queryset
+        elif self.value() == 1:
+            return queryset.filter(resolved=True)
+        else:
+            return queryset.filter(resolved=False)
+
 
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ("timestamp", "__unicode__", "content", "resolved",)
     list_links = ("timestamp", "__unicode__")
     list_editable = ("resolved",)
-    list_filter = ("resolved", "request_path", "view", "timestamp",)
+    list_filter = (ResolvedListFilter, "request_path", "view", "timestamp",)
 
     fieldsets = (
         (None, {
