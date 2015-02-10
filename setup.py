35c92276209f621b8722abd3802bc1b824c990bc
Harris Lapiroff <harris@chromamine.com>
2015-02-10 13:33:07 -0500

    Made installable and added package metadata files.

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+__version__ = __import__('feedback').__version__
+
+
+description = "A pluggable app for adding an AJAX feedback form to Django-powered websites."
+
+
+setup(
+    name="django-feedback",
+    version='.'.join([str(v) for v in __version__]),
+    url="http://github.com/littleweaver/django-feedback",
+    description=description,
+    long_description=description,
+    author='Little Weaver Web Collective, LLC',
+    author_email='hello@littleweaverweb.com',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    platforms=['OS Independent'],
+    install_requires=[
+        'django>=1.7',
+    ]
+)
